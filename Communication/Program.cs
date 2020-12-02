15cfa2fc7324003b47705df50f7cfecf1a278ccc
Unknown <theo.delettre@gmail.com>
2020-12-02 10:33:45 +0100

    Project Creation

---
+++ Communication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Communication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
