a64abc4b0fbfdb2ba55a70461a9c4d248b6aaf29
Unknown <theo.delettre@gmail.com>
2020-12-13 19:10:03 +0100

    Basic Server Users

---
+++ ChatApp/User.cs
@@ -0,0 +1,24 @@
+﻿namespace ChatApp
+{
+    public class User
+    {
+        private string _name;
+        private string _password;
+
+        public User(string name, string password)
+        {
+            _name = name;
+            _password = password;
+        }
+
+        public bool Authenticate(string uName, string uPass)
+        {
+            return uName.Equals(_name) && uPass.Equals(_password);
+        }
+
+        public bool checkName(string uName)
+        {
+            return uName.Equals(_name);
+        }
+    }
+}
---
+++ ChatApp/UserList.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace ChatApp
+{
+    public class UserList
+    {
+        private List<User> _users;
+
+        public UserList()
+        {
+            _users = new List<User>();
+        }
+
+        public bool CreateUser(string uName, string uPass)
+        {
+            if (FindUser(uName)!= null) return false;
+
+            _users.Add(new User(uName, uPass));
+
+            return true;
+        }
+
+        public User FindUser(string uName)
+        {
+            foreach (var u in _users)
+            {
+                if (u.checkName(uName))
+                {
+                    return u;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Login(string uName, string uPass)
+        {
+            foreach (User u in _users)
+            {
+                if (u.Authenticate(uName, uPass)) return true;
+            }
+
+            return false;
+        }
+    }
+}
